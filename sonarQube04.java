780f17fcc2f4903235f60fbaccfe58609b90f816
Leiiph <60736065+Leiiph@users.noreply.github.com>
2021-12-03 14:48:46 +0100

    Update sonarQube04.java

--- sonarQube04.java
+++ sonarQube04.java
@@ -1,6 +1,6 @@
 public class sonarQube04 {
     private int x;
     private int y = 0;
-    int getX() { return y;}
-    void setX(int val){ y = val; }
+    int get() { return y;}
+    void set(int val){ y = val; }
 }
